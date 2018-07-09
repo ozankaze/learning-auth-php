269f61421c4c03feb578f8e4ada8b41e96e5b26a
ozankaze <ozankaze101@gmail.com>
2018-07-09 21:10:28 +0700

    Mengamankan Form Register

--- functions/user.php
+++ functions/user.php
@@ -3,10 +3,15 @@
 function register_user($nama, $pass) {
     
     global $link;
+    // mencegah slq injection
+    $nama = mysqli_real_escape_string($link, $nama);
+    $pass = mysqli_real_escape_string($link, $pass);
     
-    $query = "INSERT INTO `users` (username, password) VALUES ('$nama', '$pass')";
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $query = "INSERT INTO `users` (`username`, `password`) VALUES ('$nama', '$pass')";
+    // var_dump($query);die();
 
-    $a = mysqli_query($link, $query);
+    // $a = mysqli_query($link, $query);
 
     // var_dump($a);die();
     if( mysqli_query($link, $query) ) {
